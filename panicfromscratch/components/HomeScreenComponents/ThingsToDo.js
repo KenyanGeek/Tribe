d8741337ee8ef078f07f0a02a4c44ac08b1d972e
Herman <chegeherman@gmail.com>
2020-03-09 21:11:20 -0400

    added a touchable highlight to my thingstodo props

--- panicfromscratch/components/HomeScreenComponents/ThingsToDo.js
+++ panicfromscratch/components/HomeScreenComponents/ThingsToDo.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
-import { Text, View, Image} from 'react-native'
+import { Text, View, Image,StyleSheet, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback} from 'react-native'
+
+
 
 export default class ThingsToDo extends Component {
+
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+  
+  _onLongPressButton() {
+    alert('You long-pressed the button!')
+  }
+
     render() {
         return (
+          <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+
             <View style={{height:200, width: 200, marginLeft: 20, borderWidth:1, borderRadius: 5, borderColor: "#dddddd"}}>
             <View style={{flex: 3}}>
               <Image source={this.props.imageUri} style={{flex: 1, width: null, height: null, resizeMode: 'cover'}}/>
@@ -13,8 +26,31 @@ export default class ThingsToDo extends Component {
                 {this.props.name}
               </Text>
             </View>
+            <View style={styles.container}>
+         
+        
+      </View>
         </View>
+        </TouchableHighlight>
+
         )
     }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 10,
+    alignItems: 'center'
+  },
+  button: {
+    marginBottom: 10,
+    width: 100,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 10,
+    color: 'white'
+  }
+});
