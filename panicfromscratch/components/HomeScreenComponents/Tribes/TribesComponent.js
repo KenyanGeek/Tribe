09f67c586bd4bfd92ec13f12a4a3d47364549ae0
Herman <chegeherman@gmail.com>
2020-03-13 10:23:56 -0400

    moved all redundant things

--- panicfromscratch/components/HomeScreenComponents/Tribes/TribesComponent.js
+++ panicfromscratch/components/HomeScreenComponents/Tribes/TribesComponent.js
@@ -1,58 +1,3 @@
-// import React from 'react';
-// import {
-//   FlatList,
-//   Text,
-//   View,
-//   Image,
-//   TouchableHighlight
-// } from 'react-native';
-// import styles from './styles';
-// import { categories } from '../../data/dataArrays';
-// import { getNumberOfRecipes } from '../../data/MockDataAPI';
-// import { NavigationContext } from '@react-navigation/native';
-
-
-
-// export default class TribesComponent extends React.Component {
-//   static navigationOptions = {
-//     title: 'Categories'
-//   };
-//   static contextType = NavigationContext;
-
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   onPressCategory = item => {
-//     const title = item.name;
-//     const category = item;
-//     navigation.navigate('TribeActivity', { category, title });
-//   };
-//   renderCategory = ({ item }) => (
-//     <TouchableHighlight underlayColor='rgba(73,182,77,1,0.9)' onPress={() => this.onPressCategory(item)}>
-//       <View style={styles.categoriesItemContainer}>
-//         <Image style={styles.categoriesPhoto} source={{ uri: item.photo_url }} /> 
-//         <Text style={styles.categoriesName}>{item.name} Tribe </Text>
-//         <Text style={styles.categoriesInfo}>{getNumberOfRecipes(item.id)} recipes</Text>
-//       </View>
-//     </TouchableHighlight>
-//   );
-
-//   render() {
-//     return (
-//       <View>
-//         <FlatList
-//           data={categories}
-//           renderItem={this.renderCategory}
-//           keyExtractor={item => `${item.id}`}
-//           horizontal={true}
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-
 import React from 'react';
 import {
   FlatList,
@@ -65,10 +10,6 @@ import styles from './styles';
 import { categories } from '../../data/dataArrays';
 import { getNumberOfRecipes } from '../../data/MockDataAPI';
 import { useNavigation } from '@react-navigation/native';
-
-
-
-
 
 
 
