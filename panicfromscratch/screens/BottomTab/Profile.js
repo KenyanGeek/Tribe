26c2cfcb4793ef4befae15506f4256719764f835
Herman <chegeherman@gmail.com>
2020-03-14 23:57:12 -0400

    experiment with timeline

--- panicfromscratch/screens/BottomTab/Profile.js
+++ panicfromscratch/screens/BottomTab/Profile.js
@@ -13,7 +13,7 @@ export default class Profile extends Component {
     this.data = [
       {time: '09:00', title: 'Event 1', description: 'Event 1 Description'},
       {time: '10:45', title: 'Event 2', description: 'Event 2 Description'},
-      {time: '12:00', title: 'Event 3', description: 'Event 3 Description'},
+      {time: '12:00', title: 'Event 3', description: 'r lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts '},
       {time: '14:00', title: 'Event 4', description: 'Event 4 Description'},
       {time: '16:30', title: 'Event 5', description: 'Event 5 Description'}
     ]
@@ -26,6 +26,15 @@ export default class Profile extends Component {
         <Timeline 
           style={styles.list}
           data={this.data}
+          circleSize={20}
+          circleColor='rgb(45,156,219)'
+          lineColor='rgb(45,156,219)'
+          timeContainerStyle={{minWidth:52, marginTop: -5}}
+          timeStyle={{textAlign: 'center', backgroundColor:'#ff9797', color:'white', padding:5, borderRadius:13}}
+          descriptionStyle={{color:'gray'}}
+          options={{
+            style:{paddingTop:5}
+          }}
         />
       </View>
     );
