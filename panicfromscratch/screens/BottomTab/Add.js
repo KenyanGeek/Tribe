96a2c682ca72b51db5a853f61e29cb11f782762c
Herman <chegeherman@gmail.com>
2020-03-20 12:12:41 -0400

    experimental tabbed screens work on add

--- panicfromscratch/screens/BottomTab/Add.js
+++ panicfromscratch/screens/BottomTab/Add.js
@@ -6,25 +6,37 @@ import Profile from '../BottomTab/Profile';
 import CommunityFeed from '../../components/DetailsComponent/TribeActivity/Feed/CommunityFeed'
 import LearningFeed from '../../components/DetailsComponent/TribeActivity/Understanding/LearningFeed'
 import ServiceFeed from '../../components/DetailsComponent/TribeActivity/Business/ServiceFeed'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 
-export default class TabsExample extends Component {
-  render() {
-    return (
-      <Container>
-        <Header hasTabs />
-        <Tabs>
-          <Tab heading="Community Chat">
-            <CommunityFeed />
-          </Tab>
-          <Tab heading="Learning">
-          <LearningFeed/>
-          </Tab>
-          <Tab heading="Services">
-          <ServiceFeed />
-          </Tab>
-        </Tabs>
-      </Container>
-    );
-  }
+
+const TopTab = createMaterialTopTabNavigator();
+
+export default function Add() {
+  return (
+    <TopTab.Navigator
+      initialRouteName="CommunityFeed"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+        labelStyle: { fontSize: 12 },
+        style: { backgroundColor: 'powderblue' },
+      }}
+    >
+      <TopTab.Screen
+        name="CommunityFeed"
+        component={CommunityFeed}
+        options={{ tabBarLabel: 'Home' }}
+      />
+      <TopTab.Screen
+        name="LearningFeed"
+        component={LearningFeed}
+        options={{ tabBarLabel: 'Updates' }}
+      />
+      <TopTab.Screen
+        name="ServiceFeed"
+        component={ServiceFeed}
+        options={{ tabBarLabel: 'Profile' }}
+      />
+    </TopTab.Navigator>
+  );
 }
